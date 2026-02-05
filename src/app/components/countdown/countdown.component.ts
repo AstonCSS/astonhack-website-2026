82e3e66c688cf21a19d61eec15ccb72e9ccf0165
JosephField09 <JosephField09@hotmail.com>
2026-02-05 18:57:00 +0000

    Added sponsors, changed countdown and added mlh banner

--- src/app/components/countdown/countdown.component.ts
+++ src/app/components/countdown/countdown.component.ts
@@ -3,42 +3,44 @@ import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 @Component({
   selector: 'app-countdown',
   standalone: true,
-  imports: [],
   templateUrl: './countdown.component.html',
   styleUrl: './countdown.component.css'
 })
 export class CountdownComponent implements AfterViewInit {
-  date: any;
-  now: any;
-  targetDate: Date = new Date("Feb 7 2026 09:00:00");
-  targetTime: any = this.targetDate.getTime();
-  difference: number;
+  targetDate = new Date('2026-02-07T10:00:00'); 
+  targetTime = this.targetDate.getTime();
 
-  @ViewChild('days', { static: true }) days: ElementRef;
-  @ViewChild('hours', { static: true }) hours: ElementRef;
-  @ViewChild('minutes', { static: true }) minutes: ElementRef;
-  @ViewChild('seconds', { static: true }) seconds: ElementRef;
-  @ViewChild('countdown-container', { static: true }) countdownContainer: HTMLDivElement;
+  @ViewChild('days', { static: true }) days!: ElementRef;
+  @ViewChild('hours', { static: true }) hours!: ElementRef;
+  @ViewChild('minutes', { static: true }) minutes!: ElementRef;
+  @ViewChild('seconds', { static: true }) seconds!: ElementRef;
 
   ngAfterViewInit(): void {
-    setInterval((): void => {
-      this.tickTock();
-      this.difference = this.targetTime - this.now;
-      this.difference = this.difference / (1000 * 60 * 60 * 24);
-
-      // I'm not sure why this works to be honest...
-      !isNaN(this.days.nativeElement.innerText)
-        ? (this.days.nativeElement.innerText = Math.floor(this.difference))
-        : (this.days.nativeElement.innerText = Math.floor(this.difference));
-    }, 1000)
+    setInterval(() => this.updateCountdown(), 1000);
   }
 
-  tickTock(): void {
-    this.date = new Date();
-    this.now = this.date.getTime();
-    this.days.nativeElement.innerText = Math.floor(this.difference);
-    this.hours.nativeElement.innerText = 23 - this.date.getHours();
-    this.minutes.nativeElement.innerText = 60 - this.date.getMinutes();
-    this.seconds.nativeElement.innerText = 60 - this.date.getSeconds();
+  updateCountdown(): void {
+    const now = new Date().getTime();
+    const diff = this.targetTime - now;
+
+    if (diff <= 0) {
+      this.days.nativeElement.innerText = '0';
+      this.hours.nativeElement.innerText = '0';
+      this.minutes.nativeElement.innerText = '0';
+      this.seconds.nativeElement.innerText = '0';
+      return;
+    }
+
+    const totalSeconds = Math.floor(diff / 1000);
+
+    const days = Math.floor(totalSeconds / (60 * 60 * 24));
+    const hours = Math.floor((totalSeconds % (60 * 60 * 24)) / (60 * 60));
+    const minutes = Math.floor((totalSeconds % (60 * 60)) / 60);
+    const seconds = totalSeconds % 60;
+
+    this.days.nativeElement.innerText = days;
+    this.hours.nativeElement.innerText = hours;
+    this.minutes.nativeElement.innerText = minutes;
+    this.seconds.nativeElement.innerText = seconds;
   }
 }
